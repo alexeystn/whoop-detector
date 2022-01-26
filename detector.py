e3eefee5173a9af9c7e1245e21a490fe8b7b212f
Alexey Stankevich <alexey.stn@gmail.com>
2022-01-26 20:43:44 +0300

    Refactoring

--- detector.py
+++ detector.py
@@ -113,54 +113,123 @@ class Beeper:
             return
 
 
+class Display:
+
+    window_name = 'Whoop Detector'
+
+    def __init__(self, config):
+        self.height = int(config['SCREEN']['height'])
+        self.width = int(config['SCREEN']['width'])
+        self.history = np.zeros((self.width,))
+        self.prev_history_value = 0
+        self.frame_counter = 0
+        self.line_height = 35
+        self.laps_list = []
+        self.max_displayed_lap_count = int(config['SCREEN']['height'])//self.line_height
+        self.show_plot = int(config['DEBUG']['show_plot'])
+        cv2.namedWindow(self.window_name, cv2.WINDOW_AUTOSIZE)
+
+    def toggle_plot(self):
+        self.show_plot = int(not self.show_plot)
+
+    def put_history(self, value):
+        self.frame_counter += 1
+        if self.frame_counter % 2 == 0:  # slow down the plot twice, keeping peak values
+            new_history_value = np.max([self.prev_history_value, value])
+            self.history[:-1] = self.history[1:]
+            self.history[-1] = new_history_value
+        self.prev_history_value = value
+
+    def draw_history_plot(self, sensitivity):
+        image = np.zeros((self.height//3, self.width, 3), dtype='uint8')
+        y = image.shape[0] // 2
+        cv2.line(image, (0, y), (image.shape[1], y), (127, 127, 127), 1)
+        y = (1 - apply_log_scale(self.history, sensitivity)) * self.height // 3
+        y[y < 3] = 3
+        y[y > self.height // 3 - 4] = self.height // 3 - 4
+        for i in range(len(self.history) - 1):
+            cv2.line(image, (i, int(y[i])), (i + 1, int(y[i + 1])), (255, 255, 255), 1, cv2.LINE_AA)
+        return image
+
+    def put_lap(self, lap_time):
+        self.laps_list.append('{0:.2f}'.format(lap_time))
+        if len(self.laps_list) > self.max_displayed_lap_count:
+            self.laps_list.pop(0)
+            self.laps_list[0] = '^'
+
+    def clear_laps(self):
+        self.laps_list = []
+
+    def show(self, parameters):
+
+        def print_text(image, text, pos, size):
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            (w, h), baseline = cv2.getTextSize(text, font, size, 2)
+            pos = (pos[0] - w // 2, pos[1] + h // 2)
+            cv2.putText(image, text, pos, font, size, [255, 255, 255], 2, cv2.LINE_AA)
+
+        def draw_laps(image, laps, line_height):
+            height = len(laps) * line_height
+            image[10:height + 10, 10:110, :] //= 2
+            for i, lap in enumerate(laps):
+                y = i * self.line_height + line_height // 2 + 10
+                print_text(image, lap, (60, y), 0.7)
+
+        def draw_pause(image):
+            (x, y) = (self.width // 2, self.height // 2)
+            image[y - 22:y + 22, x - 70:x + 70, :] //= 2
+            print_text(image, 'Pause', (x, y), 1)
+
+        def draw_mask(image, mask):
+            x_edges, y_edges = np.where(mask)
+            p0 = (np.min(y_edges) + 1, np.min(x_edges) + 1)
+            p1 = (np.max(y_edges) - 1, np.max(x_edges) - 1)
+            cv2.rectangle(image, p0, p1, color=(0, 255, 0), thickness=2)
+
+        if parameters['result'] and self.show_plot:
+            draw_mask(parameters['image'], parameters['mask'])
+
+        draw_laps(parameters['image'], self.laps_list, self.line_height)
+
+        self.put_history(parameters['estimation'])
+
+        if parameters['paused']:
+            draw_pause(parameters['image'])
+
+        if self.show_plot:
+            img_output_plot = self.draw_history_plot(parameters['sensitivity'])
+            img_output = np.vstack((parameters['image'], img_output_plot))
+        else:
+            img_output = parameters['image']
+
+        cv2.imshow(self.window_name, img_output)
+
+
 class Detector:
 
     def __init__(self, config):
         self.paused = False
         self.resolution = (int(config['SCREEN']['width']), int(config['SCREEN']['height']))
         self.sensitivity = int(config['DETECTION']['sensitivity'])
-        self.show_plot = int(config['DEBUG']['show_plot'])
+
         self.pointer = 0
         self.buffer_length = 5
         self.img_color_last = None
         self.buffer = np.zeros((self.buffer_length, self.resolution[1],
                                 self.resolution[0]), dtype='uint8')
-        self.history = np.zeros((self.resolution[0],))
-        self.prev_history_value = 0
-        self.time_to_save = 0
-        self.frame_counter = 0
-        self.line_height = 35
-        self.laps_list = []
-        self.max_displayed_lap_count = self.resolution[1]//self.line_height
 
     def toggle_pause(self):
         self.paused = not self.paused
 
-    def toggle_plot(self):
-        self.show_plot = int(not self.show_plot)
-        self.time_to_save = time.time() + 3
-
     def decrease_sensitivity(self):
         if self.sensitivity > 1:
             self.sensitivity -= 1
-            self.time_to_save = time.time() + 3
         print('Sensitivity: {0}'.format(self.sensitivity))
 
     def increase_sensitivity(self):
         if self.sensitivity < 20:
             self.sensitivity += 1
-            self.time_to_save = time.time() + 3
         print('Sensitivity: {0}'.format(self.sensitivity))
-
-    def check_config_status(self, config):
-        if self.time_to_save:
-            if self.time_to_save < time.time():
-                self.time_to_save = 0
-                config['DETECTION']['sensitivity'] = str(self.sensitivity)
-                config['DEBUG']['show_plot'] = str(self.show_plot)
-                with open('config.ini', 'w') as configfile:
-                    config.write(configfile)
-                    print('Config saved')
 
     def put_image(self, img):
         self.pointer += 1
@@ -174,62 +243,7 @@ class Detector:
         self.buffer[self.pointer, :, :] = img_gray
         self.img_color_last = cv2.resize(img, self.resolution)
 
-    def put_lap(self, lap_time):
-        self.laps_list.append('{0:.2f}'.format(lap_time))
-        if len(self.laps_list) > self.max_displayed_lap_count:
-            self.laps_list.pop(0)
-            self.laps_list[0] = '^'
-
-    def clear_laps(self):
-        self.laps_list = []
-
-    def apply_log_scale(self, values):
-        return np.log(values * np.exp((self.sensitivity - 5)/3) * (np.e - 1) + 1)
-
-    def put_history(self, value):
-        self.frame_counter += 1
-        if self.frame_counter % 2 == 0:  # slow down the plot twice, keeping peak values
-            new_history_value = np.max([self.prev_history_value, value])
-            self.history[:-1] = self.history[1:]
-            self.history[-1] = new_history_value
-        self.prev_history_value = value
-
-    def draw_history_plot(self):
-        image = np.zeros((self.resolution[1] // 3, self.resolution[0], 3), dtype='uint8')
-        y = image.shape[0] // 2
-        cv2.line(image, (0, y), (image.shape[1], y), (127, 127, 127), 1)
-        y = (1 - self.apply_log_scale(self.history)) * self.resolution[1] // 3
-        y[y < 3] = 3
-        y[y > self.resolution[1] // 3 - 4] = self.resolution[1] // 3 - 4
-        for i in range(len(self.history) - 1):
-            cv2.line(image, (i, int(y[i])), (i + 1, int(y[i + 1])), (255, 255, 255), 1, cv2.LINE_AA)
-        return image
-
     def estimate_movement(self):
-
-        def print_text(image, text, pos, size):
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            (w, h), baseline = cv2.getTextSize(text, font, size, 2)
-            pos = (pos[0] - w//2, pos[1] + h//2)
-            cv2.putText(image, text, pos, font, size, [255, 255, 255], 2, cv2.LINE_AA)
-
-        def draw_laps(image, laps, line_height):
-            height = len(laps) * line_height
-            image[10:height + 10, 10:110, :] //= 2
-            for i, lap in enumerate(laps):
-                y = i * self.line_height + line_height // 2 + 10
-                print_text(image, lap, (60, y), 0.7)
-
-        def draw_pause(image, resolution):
-            (x, y) = (resolution[0] // 2, resolution[1] // 2)
-            image[y - 22:y + 22, x - 70:x + 70, :] //= 2
-            print_text(image, 'Pause', (x, y), 1)
-
-        def draw_mask(image, mask):
-            x_edges, y_edges = np.where(mask)
-            p0 = (np.min(y_edges)+1, np.min(x_edges)+1)
-            p1 = (np.max(y_edges)-1, np.max(x_edges)-1)
-            cv2.rectangle(image, p0, p1, color=(0, 255, 0), thickness=2)
 
         threshold_difference_level = 7  # pixel brightness 0..255
 
@@ -239,27 +253,18 @@ class Detector:
         mask = img_diff >= threshold_difference_level
         n_detected_points = np.count_nonzero(mask)
         detected_points_part = n_detected_points / np.prod(self.resolution)
-        self.put_history(detected_points_part)
-        result = self.apply_log_scale(detected_points_part) > 0.5
-
+        result = apply_log_scale(detected_points_part, self.sensitivity) > 0.5
         img_output_video = self.img_color_last.copy()
-
-        if result and self.show_plot:
-            draw_mask(img_output_video, mask)
-
-        draw_laps(img_output_video, self.laps_list, self.line_height)
 
         if self.paused:
             result = False
-            draw_pause(img_output_video, self.resolution)
 
-        if self.show_plot:
-            img_output_plot = self.draw_history_plot()
-            img_output = np.vstack((img_output_video, img_output_plot))
-        else:
-            img_output = img_output_video
-
-        return result, img_output
+        return {'image': img_output_video,
+                'estimation': detected_points_part,
+                'mask': mask,
+                'sensitivity': self.sensitivity,
+                'paused': self.paused,
+                'result': result}
 
 
 class Debug:
@@ -299,18 +304,37 @@ class Debug:
                 1 / avg_cycle_time))
 
 
+class Config(configparser.ConfigParser):
+
+    filename = 'config.ini'
+
+    def __init__(self):
+        super().__init__()
+        self.read(self.filename)
+
+    def save(self, display, detector):
+        self['DETECTION']['sensitivity'] = str(detector.sensitivity)
+        self['DEBUG']['show_plot'] = str(display.show_plot)
+        with open(self.filename, 'w') as configfile:
+            self.write(configfile)
+            print('Config saved')
+
+
+def apply_log_scale(values, scale):
+    return np.log(values * np.exp((scale - 5) / 3) * (np.e - 1) + 1)
+
+
 def main():
 
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    config = Config()
     timer = Timer(config)
     capturer = Capturer(config)
     detector = Detector(config)
+    display = Display(config)
     logger = Logger()
     debug = Debug(config)
     beeper = Beeper(config)
 
-    cv2.namedWindow('Whoop Detector', cv2.WINDOW_AUTOSIZE)
     beeper.beep()
 
     while True:
@@ -318,19 +342,19 @@ def main():
         img = capturer.get_frame()
         debug.tic()
         detector.put_image(img)
-        detection_result, img_output = detector.estimate_movement()
+        args = detector.estimate_movement()
 
-        if detection_result:
+        if args['result']:
             timer_result, lap_time = timer.put_event()
             if timer_result:
                 beeper.beep()
                 if lap_time:
                     logger.put('{0:.2f}'.format(lap_time))
-                    detector.put_lap(lap_time)
+                    display.put_lap(lap_time)
                 else:
                     logger.put('Start')
 
-        cv2.imshow('Whoop Detector', img_output)
+        display.show(args)
         debug.toc()
 
         key = cv2.waitKey(1)
@@ -344,16 +368,16 @@ def main():
         elif key == 61:  # plus
             detector.increase_sensitivity()
         elif key == ord('c'):
-            detector.clear_laps()
+            display.clear_laps()
             timer.reset()
         elif key == ord('p'):
-            detector.toggle_plot()
+            display.toggle_plot()
 
-        detector.check_config_status(config)
         debug.cycle()
 
     capturer.close()
     logger.close()
+    config.save(display, detector)
 
 
 if __name__ == '__main__':
